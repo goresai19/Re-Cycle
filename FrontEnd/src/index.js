31fcedadd5b5fb8ffcafe18e8b2f651a6cadceca
Sainath Gore <100403114+goresai19@users.noreply.github.com>
2023-12-05 19:47:36 +0530

    Create index.js

---
+++ FrontEnd/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+import "./index.css";
+import App from "./App"
+
+
+
+
+
+//Importing the important functions 
+ReactDOM.render(
+   <>
+   <App/>
+   </>,
+   document.getElementById("root")
+);
