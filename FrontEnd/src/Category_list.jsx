b1c0df4e993e12006f321a9d5512fd1a533bd84c
Sainath Gore <100403114+goresai19@users.noreply.github.com>
2023-12-05 22:34:58 +0530

    Add files via upload

---
+++ FrontEnd/src/Category_list.jsx
@@ -0,0 +1,8 @@
+const Category = [
+  { id: "1", Name: "Temprature Exchange" },
+  { id: "2", Name: "Screnn,Monitors" },
+  { id: "3", Name: "Lamps" },
+  { id: "4", Name: "Large Equipments" },
+  { id: "5", Name: "Small Equipments" },
+  { id: "6", Name: "Telecommunication" }
+];
